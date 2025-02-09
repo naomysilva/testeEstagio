50a569fa24d49cb4d5c86bdd7724fd077f6e0270
NaomyAmorim <naomyleticia92@gmail.com>
2025-02-08 22:06:50 -0300

    contadorDePrimo

---
+++ LogicaDeProgramacao/contadorPrimos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int acumulador = 0; 
+        int numero; 
+
+        do
+        {
+            Console.Write("Digite um número (ou 0 para sair): ");
+            string input = Console.ReadLine();
+            numero = int.Parse(input); 
+
+            if (numero % 2 != 0 && numero != 0) 
+            {
+                acumulador++;
+            }
+        }
+        while (numero != 0); 
+
+        Console.WriteLine($"Quantidade de números ímpares digitados: {acumulador}");
+    }
+}
