e58f98e6d4eb086e6d4a601843572a2e43caa687
NaomyAmorim <naomyleticia92@gmail.com>
2025-02-08 21:10:29 -0300

    fatorial

---
+++ LogicaDeProgramacao/fatorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Digite um número: ");
+        int fatorial = 1;
+        int numero = int.Parse(Console.ReadLine());
+
+        for (int i = 2; i <= numero; i++)
+        {
+            fatorial *= i;
+        }
+
+        Console.WriteLine($"O fatorial de {numero}: {fatorial}");
+    }
+}
